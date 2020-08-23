7575004759fdb29f0ce15b82c2ddec84158ded2a
aschweizer <anton.schweizer@loeffelhardt.de>
2020-08-23 07:32:02 +0200

    step 2: add option 'format' and arguments <filepath1> <filepath2>

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
-import Command from 'commander';
+import commander from 'commander';
 
-const program = Command.createCommand();
+const program = commander.program;
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.2');
+
+program
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
+
+if(program.format) console.log(' - %s format', program.format);
+
