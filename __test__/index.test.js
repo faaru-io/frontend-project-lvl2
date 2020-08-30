36c803ce04d0bf99fb3cc149e26ce69fd9e60231
aschweizer <anton.schweizer@loeffelhardt.de>
2020-08-30 09:50:44 +0200

    step 4: correct linter annotations eslint no-underscore-dangle

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,9 +1,10 @@
 import { fileURLToPath } from 'url';
-import { test, expect } from "@jest/globals";
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
+/* eslint no-underscore-dangle: [2, { "allow": ["__filename", "__dirname"] }] */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
