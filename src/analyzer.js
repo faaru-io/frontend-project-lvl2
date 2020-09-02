75d54fba922bd7d221cac101c50b48d15552b8eb
aschweizer <anton.schweizer@loeffelhardt.de>
2020-09-02 22:59:10 +0200

    step 7: build recursive ast

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,33 +1,51 @@
 import _ from 'lodash';
 
+const isObjectOnly = (variable) => {
+  return variable && typeof variable === 'object' && !Array.isArray(variable);
+};
+
 const buildAst = (config1, config2) => {
-  const keysOfConfig1 = Object.keys(config1);
-  const keysOfConfig2 = Object.keys(config2);
-  const unionConfigKeys = _.union(keysOfConfig1, keysOfConfig2);
-  const sortedUnionConfigKeys = unionConfigKeys.sort();
+  const iter = (child1, child2, state = null) => {
+    const keysOfConfig1 = Object.keys(child1);
+    const keysOfConfig2 = Object.keys(child2);
+    const unionConfigKeys = _.union(keysOfConfig1, keysOfConfig2);
+    const sortedUnionConfigKeys = unionConfigKeys.sort();
 
-  return sortedUnionConfigKeys.map((key) => {
-    const value1 = _.get(config1, key, null);
-    const value2 = _.get(config2, key, null);
+    return sortedUnionConfigKeys.map((key) => {
+      const value1 = _.get(child1, key, null);
+      const value2 = _.get(child2, key, null);
 
-    let state;
-    if (!_.has(config1, key)) {
-      state = 'added';
-    } else if (!_.has(config2, key)) {
-      state = 'deleted';
-    } else if (value1 !== value2) {
-      state = 'changed';
-    } else {
-      state = 'unchanged';
-    }
+      if (isObjectOnly(value1) && isObjectOnly(value2)) {
+        const value = iter(value1, value2);
+        return {
+          key,
+          value1: value,
+          value2: value,
+          state: 'unchanged',
+        };
+      }
 
-    return {
-      key,
-      value1,
-      value2,
-      state,
-    };
-  });
+      let state;
+      if (isObjectOnly(value1) && isObjectOnly(value2) || value1 === value2) {
+        state = 'unchanged';
+      } else if (!_.has(child1, key)) {
+        state = 'added';
+      } else if (!_.has(child2, key)) {
+        state = 'deleted';
+      } else {
+        state = 'changed';
+      }
+
+      return {
+        key,
+        value1: isObjectOnly(value1) ? iter(value1, value2) : value1,
+        value2: isObjectOnly(value2) ? iter(value1, value2) : value2,
+        state,
+      };
+    });
+  };
+
+  return iter(config1, config2, '');
 };
 
 export default buildAst;
