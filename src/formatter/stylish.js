90b3593d5536a184f220e2eba7f19a0128c2f402
aschweizer <anton.schweizer@loeffelhardt.de>
2020-09-15 23:11:25 +0200

    step 9: refactoring code smell

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const buildSpaces = (count) => ' '.repeat(count * 2);
 
-const myStringify = (node, level) => {
+const normalizeNodeValue = (node, level) => {
   if (!_.isObject(node)) {
     return node;
   }
@@ -10,38 +10,35 @@ const myStringify = (node, level) => {
   const entries = Object.entries(node);
   const spaces = buildSpaces(level);
   const result = entries.flatMap(([key, value]) => {
-    const subNode = myStringify(value, level + 2);
+    const subNode = normalizeNodeValue(value, level + 2);
     return [`${spaces}    ${key}: ${subNode}`];
   });
 
   return ['{', ...result, `${spaces}}`].join('\n');
 };
 
-const mapState = {
-  added: (node, level) => {
-    const spaces = buildSpaces(level);
-    const value = myStringify(node.value, level + 1);
-    return [`${spaces}+ ${node.key}: ${value}`];
-  },
-  deleted: (node, level) => {
-    const spaces = buildSpaces(level);
-    const value = myStringify(node.value, level + 1);
-    return [`${spaces}- ${node.key}: ${value}`];
-  },
-  unchanged: (node, level) => {
-    const spaces = buildSpaces(level);
-    const value = myStringify(node.value, level + 1);
-    return [`${spaces}  ${node.key}: ${value}`];
-  },
-  changed: (node, level) => {
-    const spaces = buildSpaces(level);
-    const oldValue = myStringify(node.value.old, level + 1);
-    const newValue = myStringify(node.value.new, level + 1);
-    return [
-      `${spaces}- ${node.key}: ${oldValue}`,
-      `${spaces}+ ${node.key}: ${newValue}`,
-    ];
-  },
+const generateKeyValueString = (key, value, sign, level) => {
+  const spaces = buildSpaces(level);
+  const normalizedValue = normalizeNodeValue(value, level + 1);
+  return `${spaces}${sign} ${key}: ${normalizedValue}`;
+};
+
+const mapType = {
+  added: (node, level) => (
+    generateKeyValueString(node.key, node.value, '+', level)
+  ),
+  deleted: (node, level) => (
+    generateKeyValueString(node.key, node.value, '-', level)
+  ),
+  unchanged: (node, level) => (
+    generateKeyValueString(node.key, node.value, ' ', level)
+  ),
+  changed: (node, level) => (
+    [
+      generateKeyValueString(node.key, node.value.old, '-', level),
+      generateKeyValueString(node.key, node.value.new, '+', level),
+    ]
+  ),
   children: (node, level, cb) => {
     const spaces = buildSpaces(level);
     return [`${spaces}  ${node.key}: {`, ...cb(node.value, level + 2), `${spaces}  }`];
@@ -50,7 +47,7 @@ const mapState = {
 
 const format = (ast) => {
   const iter = (nodes, level) => (
-    nodes.flatMap((node) => mapState[node.type](node, level, iter))
+    nodes.flatMap((node) => mapType[node.type](node, level, iter))
   );
 
   return ['{', ...iter(ast, 1), '}'].join('\n');
