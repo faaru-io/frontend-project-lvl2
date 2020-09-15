ad22af00b42ba71b90761df822be2f770329f03c
aschweizer <anton.schweizer@loeffelhardt.de>
2020-09-15 22:21:39 +0200

    step 9: refactoring stylish formatter

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const buildSpaces = (count) => ' '.repeat(count);
+const buildSpaces = (count) => ' '.repeat(count * 2);
 
 const myStringify = (node, level) => {
   if (!_.isObject(node)) {
@@ -8,40 +8,50 @@ const myStringify = (node, level) => {
   }
 
   const entries = Object.entries(node);
-  const spaces = buildSpaces(level * 2);
-  const result = entries.map(([key, value]) => {
+  const spaces = buildSpaces(level);
+  const result = entries.flatMap(([key, value]) => {
     const subNode = myStringify(value, level + 2);
-    return `${spaces}    ${key}: ${subNode}`;
-  }).flat();
+    return [`${spaces}    ${key}: ${subNode}`];
+  });
 
   return ['{', ...result, `${spaces}}`].join('\n');
 };
 
 const mapState = {
-  added: (key, value, spaces) => `${spaces}+ ${key}: ${value}`,
-  deleted: (key, value, spaces) => `${spaces}- ${key}: ${value}`,
-  unchanged: (key, value, spaces) => `${spaces}  ${key}: ${value}`,
+  added: (node, level) => {
+    const spaces = buildSpaces(level);
+    const value = myStringify(node.value, level + 1);
+    return [`${spaces}+ ${node.key}: ${value}`];
+  },
+  deleted: (node, level) => {
+    const spaces = buildSpaces(level);
+    const value = myStringify(node.value, level + 1);
+    return [`${spaces}- ${node.key}: ${value}`];
+  },
+  unchanged: (node, level) => {
+    const spaces = buildSpaces(level);
+    const value = myStringify(node.value, level + 1);
+    return [`${spaces}  ${node.key}: ${value}`];
+  },
+  changed: (node, level) => {
+    const spaces = buildSpaces(level);
+    const oldValue = myStringify(node.value.old, level + 1);
+    const newValue = myStringify(node.value.new, level + 1);
+    return [
+      `${spaces}- ${node.key}: ${oldValue}`,
+      `${spaces}+ ${node.key}: ${newValue}`,
+    ];
+  },
+  children: (node, level, cb) => {
+    const spaces = buildSpaces(level);
+    return [`${spaces}  ${node.key}: {`, ...cb(node.value, level + 2), `${spaces}  }`];
+  },
 };
 
 const format = (ast) => {
-  const iter = (nodes, level) => {
-    const spaces = buildSpaces(level * 2);
-    return nodes.flatMap((node) => {
-      if (node.type === 'children') {
-        return [`${spaces}  ${node.key}: {`, ...iter(node.value, level + 2), `${spaces}  }`];
-      }
-
-      if (node.type === 'changed') {
-        return [
-          mapState.deleted(node.key, myStringify(node.value.old, level + 1), spaces),
-          mapState.added(node.key, myStringify(node.value.new, level + 1), spaces),
-        ];
-      }
-
-      const formatState = mapState[node.type];
-      return formatState(node.key, myStringify(node.value, level + 1), spaces);
-    });
-  };
+  const iter = (nodes, level) => (
+    nodes.flatMap((node) => mapState[node.type](node, level, iter))
+  );
 
   return ['{', ...iter(ast, 1), '}'].join('\n');
 };
